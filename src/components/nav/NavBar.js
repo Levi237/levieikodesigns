e7a8347137bb30eaea5f215872193925d034e17c
Levi237 <levieiko@gmail.com>
2023-08-12 02:25:11 -0700

    change style direction to className

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -6,17 +6,17 @@ import * as routes from '../../constants/routes';
 import { ReactComponent as LogoSVG }      from '../../svg/brand/logo.svg';
 // import { ReactComponent as HamburgerSVG } from '../../svg/icons/hamburger.svg';
 
-const NavBar = ({ btnHamburger, toggleMenu, pageStyle }) => {
-  return(<>
-    <Logo>
+const NavBar = ({ btnHamburger, toggleMenu, designStyle }) => {
+  return(<div>
+    <Logo name="logo" className={"nav-bar " + (designStyle)}>
       <NavLink to={routes.ROOT}>
-        <LogoSVG className="" style={{fill: pageStyle.color, display: pageStyle.displayLogo}}/>
+        <LogoSVG/>
       </NavLink>
     </Logo>
-    <Hamburger name="toggleMenu" onClick={(e) => {toggleMenu(e); btnHamburger(); }}>
-      <nav id="hamburger-btn" className=""><section style={{background: pageStyle.color}}></section><section style={{background: pageStyle.color}}></section></nav>
+    <Hamburger name="toggleMenu" className={"nav-bar " + (designStyle)} onClick={(e) => {toggleMenu(e); btnHamburger(); }}>
+      <nav id="hamburger-btn"><section></section><section></section></nav>
     </Hamburger>
-  </>);
+  </div>);
 };
 
 const GlobalDiv = styled.div`
