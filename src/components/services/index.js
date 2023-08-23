c8037b9ee99fde90fcf1c2f93edb0478defce49d
Levi237 <levieiko@gmail.com>
2023-08-23 12:32:39 -0700

    clean up state

--- src/components/services/index.js
+++ src/components/services/index.js
@@ -14,19 +14,19 @@ export default class ServicesPage extends Component {
         paragraph: `Emailing your customers is an excellent way to maintain a positive relationship while promoting specials they might be interested in.  It's all about tact.  Knowing what to send to who and when is important.  The better you target you audiences the more effective you can be at driving up sales but now and in the long term.`,
         listName: `What I do:`,
         checklist: [
-          'Target audiences to promote sales & new products',
-          'Upgrade your professional look',
-          'Feature custom graphic designs',
-          'Bypass the spam, get into the Inbox',
-          'Create value-added content to build trust'
+          `Target audiences to promote sales & new products`,
+          `Upgrade your professional look`,
+          `Feature custom graphic designs`,
+          `Bypass the spam, get into the Inbox`,
+          `Create value-added content to build trust`
         ],
         collage: [
-          'clients/coveredperfectly_3.png', 
-          'clients/baddie-by-divinity_2.jpg',
-          'clients/bravo-built_3.jpg'
+          `clients/coveredperfectly_3.png`, 
+          `clients/baddie-by-divinity_2.jpg`,
+          `clients/bravo-built_3.jpg`
         ],
         position: {
-          text: 'right bottom'
+          text: `right bottom`
         }
       },
       graphics: {
@@ -36,18 +36,18 @@ export default class ServicesPage extends Component {
         paragraph: `I help businesses create interesting, effective imagery for an elevated user experience.  Whether it's making your idea a reality, or coming up with a whole new look, I can help.`,
         listName: `What I do:`,
         checklist: [
-          'Graphics / photoshop touchups',
-          'Product imagery & packaging',
-          'Marketing / advertising campaigns',
-          'Logos & brand identity development'
+          `Graphics / photoshop touchups`,
+          `Product imagery & packaging`,
+          `Marketing / advertising campaigns`,
+          `Logos & brand identity development`
         ],
         collage: [
-          'services/red5-collection.png',
-          'services/bravo-video.png', 
-          'services/divinity-youtube.png'
+          `services/red5-collection.png`,
+          `services/bravo-video.png`, 
+          `services/divinity-youtube.png`
         ],
         position: {
-          text: 'left bottom'
+          text: `left bottom`
         }
       },
       additions: {
@@ -57,18 +57,18 @@ export default class ServicesPage extends Component {
         paragraph: `Maybe you already have a website but your "Current Theme" doesn't have the Section style in your Template you want. Maybe you want a holiday promotional banner set up to capture a little extra revenue this season.`,
         listName: `What I do:`,
         checklist: [
-          'Build customer friendly editable & reusable templates',
-          'Custom coded unique sections inserted directly onto a page',
-          'Utilize existing templates to design stylish presentations',
-          'Install, integrate and personalize third party apps',
+          `Build customer friendly editable & reusable templates`,
+          `Custom coded unique sections inserted directly onto a page`,
+          `Utilize existing templates to design stylish presentations`,
+          `Install, integrate and personalize third party apps`,
         ],
         collage: [
-          'services/custom-red5-section.jpg', 
-          'services/custom-red5-file.jpg', 
-          'services/custom-nexus-section.jpg', 
+          `services/custom-red5-section.jpg`, 
+          `services/custom-red5-file.jpg`, 
+          `services/custom-nexus-section.jpg`, 
         ],
         position: {
-          text: 'right bottom'
+          text: `right bottom`
         }
       },
       websites: {
@@ -78,18 +78,18 @@ export default class ServicesPage extends Component {
         paragraph: `For many small businesses, starting an online store is a big step.  You have a great idea, you have the market, all you need now is to open shop!  Not only do I build websites, but I help get your whole digital storefront up and running.`,
         listName: `What I do:`,
         checklist: [
-          'Design & develop eCommerce websites & blogs',
-          'Connect third party accounts, including email',
-          'Assist in product design, cover images, etc.',
-          'Create marketing strategies to capture leads & drive sales',
+          `Design & develop eCommerce websites & blogs`,
+          `Connect third party accounts, including email`,
+          `Assist in product design, cover images, etc.`,
+          `Create marketing strategies to capture leads & drive sales`,
         ],
         collage: [
-          'services/divinity-home.png', 
-          'services/metamorphosis-banner.png',
-          'services/momo-quiz.png',
+          `services/divinity-home.png`, 
+          `services/metamorphosis-banner.png`,
+          `services/momo-quiz.png`,
         ],
         position: {
-          text: 'left bottom',
+          text: `left bottom`,
         }
       }
     }
