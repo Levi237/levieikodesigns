114678c26e8bc66a50ef342f7d9c667014e36458
Levi237 <levieiko@gmail.com>
2023-08-24 15:11:01 -0700

    move to new file to iO effect

---
+++ src/components/clients/ClientSection.js
@@ -0,0 +1,212 @@
+// PROJECT NOTES *************************************************************
+// copy the DisplaySection example
+// pass client instead, must map in parent.
+// goak: when scroll into view, intersectionObserver triggers for each section
+
+import React, { useEffect, useRef, useState } from 'react';
+import styled               from 'styled-components';
+
+const ClientSection = ({ client }) => {
+  // Add intersection observer to the section 
+  const intersectionRef = useRef();
+  const [refIntersection, setRefIntersection] = useState();
+  console.log("refIntersection", refIntersection);
+  useEffect(() => {
+      console.log('intersectionRef', intersectionRef.current);
+      const observer = new IntersectionObserver((entries) => {
+          const entry = entries[0];
+          setRefIntersection(entry.isIntersecting);
+          if (entry.isIntersecting){
+            entry.target.classList.remove('hide-intersection');
+            console.log('interested', entry.target);
+            observer.disconnect();
+          }
+          console.log('entry', entry.target.className);
+      });
+      observer.observe(intersectionRef.current);
+  }, []);
+    // const mapChecklist = service.checklist.map((item, k) => {
+    //   return <section key={k}><div>✔</div><div>{ item }</div></section>
+    // })
+    // const mapCollage = service.collage.map((item, k) => {
+    //   return <div key={k}><img src={`./${item}`} alt="collage one"/></div>
+    // })
+    const tasksList = client.tasks.map((r, k) => {
+        return <li key={k}><span>{r}</span></li>
+    });
+    console.log('client', client)
+    return(
+          <Wrapper ref={intersectionRef} className={`hide-intersection`}>
+                <MobileImg href={`https://${client.link}`} target="_blank">
+                    <img src={client.image} alt={client.title}/>
+                </MobileImg>
+                <section>
+                    <h2>
+                        {client.title}&nbsp;<small className="no-break">{client.startDate}</small>
+                    </h2>
+                    <a target="_blank" rel="noopener noreferrer" href={`https://${client.link}`}>
+                        {client.link}
+                    </a>
+                    <p>
+                        “{client.statement}”
+                    </p>
+                    <MobileColumns className="mobile-columns">
+                        <ul>
+                            {tasksList}
+                        </ul>
+                        {client.image2 && <img className="mobile-block" src={client.image2} alt={client.title}/>}
+                    </MobileColumns>
+                    <br></br>
+                </section>
+                <DesktopImg href={`https://${client.link}`} target="_blank">
+                    <img src={client.image} alt={client.title}/>
+                    {client.image2 && <img className="image-2" src={client.image2} alt={client.title}/>}
+                </DesktopImg>
+                <hr/>
+          </Wrapper>
+    );
+  };
+
+
+const Wrapper = styled.div`
+// animation for interestionObserver class.remove('hide-intersecion')
+    opacity: 1;
+    // margin-top: 0;
+    transition: 1.6s ease;
+
+    margin: 0 auto;
+    padding: 20px 0;
+    max-width: 800px;
+    text-align: center;
+
+    section {
+        width: 40%;
+        padding: 0 5%;
+        max-width: 400px;
+        display: inline-block;
+        vertical-align: top;
+        text-align: left;
+        span::after {
+            content ' | ';
+        }
+        span:last-of-type::after {
+            content: '';
+        }
+    }
+    
+    > a {
+        width: 50%;
+        max-width: 400px;
+        img {
+            width: 100%;
+            box-shadow: 0 1px 12px rgba(0,0,0,.2);
+        }
+    }
+    // &:nth-of-type(odd) > a {
+    //     float: left;
+    // }
+
+    a {
+        color: var(--blue)!important;
+        text-decoration: none;
+        margin-bottom: 40px;
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+    h1 {
+        margin-bottom: 10px!important;
+    }
+    h2 {
+        font-weight: 500;
+        small {
+            font-weight: 100;
+        }
+    }
+    h3 {
+        font-size: 18px;
+    }
+    p {
+        font-size: 16px;
+        margin: 10px auto;
+    }
+    ul, span {
+        font-size: 16px;
+    }
+    ul {
+        margin: 10px auto!important;
+        line-height: 140%;
+    }
+    >hr {
+        width: 100%;
+        margin: 0px auto 20px;
+    }
+
+    section > hr {
+        display: none;
+    }
+    @media screen and (max-width: 900px) {
+        section {
+            width: 90%;
+            margin-top: 16px;
+        }
+        > a {
+            width: 90%;
+            max-width: 400px;
+            margin: 5% auto 0;
+            img {
+                width: 100%;
+                box-shadow: 0 1px 12px rgba(0,0,0,.2);
+            }
+        }
+        > hr {
+            display: none;
+        }
+        section > hr {
+            display: block;
+            width: 80%;
+            margin: 5% auto;
+        }
+        ul {
+            padding-left: 24px;
+        }
+        li span {
+            position: relative;
+            left: -5px;
+          }
+    }
+`;
+const DesktopImg = styled.a`
+    display: inline-block;
+    position: relative;
+    @media screen and (max-width: 900px) {
+        display: none;
+    }
+    .image-2 {
+        position: absolute;
+        left: 0;
+        opacity: 0;
+        transition: .3s ease;
+    }
+    &:hover {
+        .image-2 {
+            opacity: 1;
+        }
+    }
+`;
+
+const MobileImg = styled.a`
+    display: none;
+    @media screen and (max-width: 900px) {
+        display: block;
+`;
+const MobileColumns = styled.div`
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 5px;
+    @media screen and (max-width: 900px) {
+        grid-template-columns: 60% 40%;
+    }
+`;
+
+export default ClientSection;
--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -1,42 +1,21 @@
+// PROJECT NOTES *************************************************************
+// copy the DisplaySection example
+// pass client instead, must map in parent.
+// goak: when scroll into view, intersectionObserver triggers for each section
+
 import React                    from 'react';
 import styled                   from 'styled-components';
+
+import ClientSection from './ClientSection';
 
 const Clients = ({ clientList }) => {
 
     const liveClients = clientList.filter(client => client.status === 'live');
     const mapClientList = liveClients.map((client, key) => {
-        const tasksList = client.tasks.map((r, k) => {
-            return <li key={k}><span>{r}</span></li>
-        });
+
         return(
-            <ClientContainer key={key}>
-                <MobileImg href={`https://${client.link}`} target="_blank">
-                    <img src={client.image} alt={client.title}/>
-                </MobileImg>
-                <section>
-                    <h2>
-                        {client.title}&nbsp;<small className="no-break">{client.startDate}</small>
-                    </h2>
-                    <a target="_blank" rel="noopener noreferrer" href={`https://${client.link}`}>
-                        {client.link}
-                    </a>
-                    <p>
-                        “{client.statement}”
-                    </p>
-                    <MobileColumns className="mobile-columns">
-                        <ul>
-                            {tasksList}
-                        </ul>
-                        {client.image2 && <img className="mobile-block" src={client.image2} alt={client.title}/>}
-                    </MobileColumns>
-                    <br></br>
-                </section>
-                <DesktopImg href={`https://${client.link}`} target="_blank">
-                    <img src={client.image} alt={client.title}/>
-                    {client.image2 && <img className="image-2" src={client.image2} alt={client.title}/>}
-                </DesktopImg>
-                <hr/>
-            </ClientContainer>
+                <ClientSection key={key} client={client}/>
+                
         );
     });
 
@@ -47,141 +26,15 @@ const Clients = ({ clientList }) => {
     );
 };
 
-const MobileColumns = styled.div`
-    display: grid;
-    grid-template-columns: 1fr;
-    gap: 5px;
-    @media screen and (max-width: 900px) {
-        grid-template-columns: 60% 40%;
-    }
-`;
+// const MobileColumns = styled.div`
+//     display: grid;
+//     grid-template-columns: 1fr;
+//     gap: 5px;
+//     @media screen and (max-width: 900px) {
+//         grid-template-columns: 60% 40%;
+//     }
+// `;
 
-const ClientContainer = styled.div`
-    margin: 40px auto;
-    max-width: 800px;
-    text-align: center;
-
-    section {
-        width: 40%;
-        padding: 0 5%;
-        max-width: 400px;
-        display: inline-block;
-        vertical-align: top;
-        text-align: left;
-        span::after {
-            content ' | ';
-        }
-        span:last-of-type::after {
-            content: '';
-        }
-    }
-    
-    > a {
-        width: 50%;
-        max-width: 400px;
-        img {
-            width: 100%;
-            box-shadow: 0 1px 12px rgba(0,0,0,.2);
-        }
-    }
-    &:nth-of-type(odd) > a {
-        float: left;
-    }
-
-    a {
-        color: var(--blue)!important;
-        text-decoration: none;
-        margin-bottom: 40px;
-        &:hover {
-            text-decoration: underline;
-        }
-    }
-    h1 {
-        margin-bottom: 10px!important;
-    }
-    h2 {
-        font-weight: 500;
-        small {
-            font-weight: 100;
-        }
-    }
-    h3 {
-        font-size: 18px;
-    }
-    p {
-        font-size: 16px;
-        margin: 10px auto;
-    }
-    ul, span {
-        font-size: 16px;
-    }
-    ul {
-        margin: 10px auto!important;
-        line-height: 140%;
-    }
-    >hr {
-        width: 100%;
-        margin: 0px auto 20px;
-    }
-
-    section > hr {
-        display: none;
-    }
-    @media screen and (max-width: 900px) {
-        > a {
-            width: 90%;
-            max-width: 400px;
-            margin: 5% auto 0;
-            img {
-                width: 100%;
-                box-shadow: 0 1px 12px rgba(0,0,0,.2);
-            }
-        }
-        &:nth-of-type(odd) > a {
-            float: none;
-        }
-        >hr {
-            display: none;
-        }
-        section > hr {
-            display: block;
-            width: 80%;
-            margin: 5% auto;
-        }
-        ul {
-            padding-left: 24px;
-        }
-        li span {
-            position: relative;
-            left: -5px;
-          }
-    }
-`;
-
-const DesktopImg = styled.a`
-    display: inline-block;
-    position: relative;
-    @media screen and (max-width: 900px) {
-        display: none;
-    }
-    .image-2 {
-        position: absolute;
-        left: 0;
-        opacity: 0;
-        transition: .3s ease;
-    }
-    &:hover {
-        .image-2 {
-            opacity: 1;
-        }
-    }
-`;
-
-const MobileImg = styled.a`
-    display: none;
-    @media screen and (max-width: 900px) {
-        display: block;
-`;
 const Container = styled.div`
     position: relative;
     hr {
@@ -199,6 +52,14 @@ const Container = styled.div`
         }
         >hr {
             display: none;
+        }
+    }
+    div:nth-of-type(odd) > a {
+        float: left;
+    }
+    @media screen and (max-width: 900px) {
+        div:nth-of-type(odd) > a {
+            float: none;
         }
     }
 `;
