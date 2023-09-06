423b9192b418a8d4d49c6fe4f07fb4a48ed0e3f0
Levi237 <levieiko@gmail.com>
2023-09-06 01:48:02 -0700

    create card for intersectionObserver

---
+++ src/components/portfolio/InactiveProjectCard.js
@@ -0,0 +1,148 @@
+// PROJECT NOTES *************************************************************
+// copy the DisplaySection example
+// pass project instead, must map in parent.
+// goak: when scroll into view, intersectionObserver triggers for each section
+
+import React, { useEffect, useRef, useState } from 'react';
+import styled                                 from 'styled-components';
+
+const InactiveProjectCard = ({ project }) => {
+// Add intersection observer to the section 
+    const ref = useRef();
+    const [refState, setRefState] = useState();
+    console.log("refState", refState);
+    useEffect(() => {
+        console.log('ref', ref.current);
+        const observer = new IntersectionObserver((entries) => {
+            const entry = entries[0];
+            setRefState(entry.isIntersecting);
+            if (entry.isIntersecting){
+            entry.target.classList.remove('hide-intersection');
+            console.log('interested', entry.target);
+            observer.disconnect();
+            }
+            console.log('entry', entry.target.className);
+        });
+        observer.observe(ref.current);
+    }, []);
+
+    const technologiesList = project.technologies.map((c, k) => {
+        return <span key={k}>{c}</span>
+    });
+    const tasksList = project.tasks.map((r, k) => {
+        return <li key={k}><span>{r}</span></li>
+    });
+    return(
+        <PastProject  ref={ref}>
+            <section>
+                <img src={project.image} alt={project.name}/>
+            </section>
+            <section>
+                <h2>{project.name}</h2>
+                <small>{project.startDate} - {project.endDate}</small>
+                <h4>{project.link}</h4>
+                <p>“{project.statement}”</p>
+                <ul>{tasksList}</ul>
+                <div>{technologiesList}</div>
+            </section>
+        </PastProject>
+    );
+};
+
+const PastProject = styled.div`
+    width: 21%;
+    margin: 40px 1%;
+    text-align: center;
+    
+    section {
+        text-align: left;
+        &:first-of-type {
+            overflow: hidden;
+            height: 13.33vw;
+            max-height: 160px;
+            margin-bottom: 20px;
+        }
+        > div {
+            display: flex;
+            flex-wrap: wrap;
+            align-items: start;
+            justify-content: start;
+        }
+        > div > span {
+            border-left: 1px solid grey;
+            border-right: 1px solid grey;
+            // border: 1px solid grey;
+            padding: 0px .85vw;
+            margin-bottom: 1px;
+        }
+        > div > span {
+            margin-right: -1px;
+        }
+    }
+    
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        // margin-top: 5%;
+    }
+    a {
+        text-decoration: none;
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+    h1 {
+        margin-bottom: 10px!important;
+    }
+    h2 {
+        font-size: 21px;
+        margin: 0 0 5px;
+    }
+
+    h4 {
+        font-size: 14px;
+        font-weight: 100;
+    }
+    p {
+        margin: 10px auto;
+        font-size: 14px;
+    }
+    ul, span {
+        font-size: 14px;
+    }
+    ul {
+        margin: 10px auto!important;
+        padding-inline-start: 12px;
+    }
+    @media screen and (max-width: 640px) {
+        width: 100%;
+        margin: 0 0%;
+        > section {
+            text-align: left;
+            &:first-of-type {
+                height: 24vw;
+                margin-bottom: 10px;
+            }
+            &:last-of-type {
+                margin-bottom: 40px;
+            }
+            > div {
+                margin-top: 5px;
+            }
+        }
+        h2 {
+            font-size: 4.6vw;
+            margin: 0;
+        }
+        h4 {
+            margin: .1vw 0;
+            letter-spacing: .1vw;
+        }
+        p, ul {
+            display: none;
+        }
+    }
+`;
+
+export default InactiveProjectCard;
--- src/components/portfolio/InactiveProjects.js
+++ src/components/portfolio/InactiveProjects.js
@@ -1,30 +1,35 @@
 import React  from 'react';
 import styled from 'styled-components';
 
+import InactiveProjectCard from './InactiveProjectCard';
+
 const PastProjects = ({ projects }) => {
     const pastProjects = projects.filter(project => project.active_project === false);
     const mapPastProjects = pastProjects.map((project, key) => {
-        const technologiesList = project.technologies.map((c, k) => {
-            return <span key={k}>{c}</span>
-        });
-        const tasksList = project.tasks.map((r, k) => {
-            return <li key={k}><span>{r}</span></li>
-        });
         return(
-            <PastProject key={key}>
-                <section>
-                    <img src={project.image} alt={project.name}/>
-                </section>
-                <section>
-                    <h2>{project.name}</h2>
-                    <small>{project.startDate} - {project.endDate}</small>
-                    <h4>{project.link}</h4>
-                    <p>“{project.statement}”</p>
-                    <ul>{tasksList}</ul>
-                    <div>{technologiesList}</div>
-                </section>
-            </PastProject>
-        );
+            <InactiveProjectCard project={project} key={key}/>
+        )
+        // const technologiesList = project.technologies.map((c, k) => {
+        //     return <span key={k}>{c}</span>
+        // });
+        // const tasksList = project.tasks.map((r, k) => {
+        //     return <li key={k}><span>{r}</span></li>
+        // });
+        // return(
+        //     <PastProject key={key}>
+        //         <section>
+        //             <img src={project.image} alt={project.name}/>
+        //         </section>
+        //         <section>
+        //             <h2>{project.name}</h2>
+        //             <small>{project.startDate} - {project.endDate}</small>
+        //             <h4>{project.link}</h4>
+        //             <p>“{project.statement}”</p>
+        //             <ul>{tasksList}</ul>
+        //             <div>{technologiesList}</div>
+        //         </section>
+        //     </PastProject>
+        // );
     });
     return(
         <Container>
@@ -60,101 +65,6 @@ const Container = styled.div`
     }
 `;
 
-const PastProject = styled.div`
-    width: 21%;
-    margin: 40px 1%;
-    text-align: center;
-    
-    section {
-        text-align: left;
-        &:first-of-type {
-            overflow: hidden;
-            height: 13.33vw;
-            max-height: 160px;
-            margin-bottom: 20px;
-        }
-        > div {
-            display: flex;
-            flex-wrap: wrap;
-            align-items: start;
-            justify-content: start;
-        }
-        > div > span {
-            border-left: 1px solid grey;
-            border-right: 1px solid grey;
-            // border: 1px solid grey;
-            padding: 0px .85vw;
-            margin-bottom: 1px;
-        }
-        > div > span {
-            margin-right: -1px;
-        }
-    }
-    
-    img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-        // margin-top: 5%;
-    }
-    a {
-        text-decoration: none;
-        &:hover {
-            text-decoration: underline;
-        }
-    }
-    h1 {
-        margin-bottom: 10px!important;
-    }
-    h2 {
-        font-size: 21px;
-        margin: 0 0 5px;
-    }
-
-    h4 {
-        font-size: 14px;
-        font-weight: 100;
-    }
-    p {
-        margin: 10px auto;
-        font-size: 14px;
-    }
-    ul, span {
-        font-size: 14px;
-    }
-    ul {
-        margin: 10px auto!important;
-        padding-inline-start: 12px;
-    }
-    @media screen and (max-width: 640px) {
-        width: 100%;
-        margin: 0 0%;
-        > section {
-            text-align: left;
-            &:first-of-type {
-                height: 24vw;
-                margin-bottom: 10px;
-            }
-            &:last-of-type {
-                margin-bottom: 40px;
-            }
-            > div {
-                margin-top: 5px;
-            }
-        }
-        h2 {
-            font-size: 4.6vw;
-            margin: 0;
-        }
-        h4 {
-            margin: .1vw 0;
-            letter-spacing: .1vw;
-        }
-        p, ul {
-            display: none;
-        }
-    }
-`;
 
 
 
